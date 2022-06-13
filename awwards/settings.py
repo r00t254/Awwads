a55751cecba221ab74176c9549739b70977e0147
r00t254 <root254.mary@gmail.com>
2022-06-13 14:32:03 +0300

    add changes

--- awwards/settings.py
+++ awwards/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-from pyuploadcare.dj import conf as dj_conf
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","app.settings")
+# exportDJANGO_SETTINGS_MODULE=awwards.settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,8 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myawwards',
     'crispy_forms',
-    'rest_framework',
-    'pyuploadcare.dj',
+    # 'rest_framework',
+    # 'pyuploadcare.dj',
     'tinymce',
     
 ]
